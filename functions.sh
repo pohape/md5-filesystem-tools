6d7d5a3c06f1daaf7a5f1932808cf2ab23a784e2
Pavel Gev <pavelgeveiler@gmail.com>
2024-03-12 23:03:47 +0100

    remove_duplicates.sh

--- functions.sh
+++ functions.sh
@@ -4,7 +4,7 @@ generate_hash_map() {
 
     while read -r line; do
         local hash=$(echo "$line" | awk '{print $1}')
-        local file=$(echo "$line" | cut -d ' ' -f 2-)
+        local file=$(echo "$line" | cut -d ' ' -f 2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
         if [[ -n ${file_hash_map[$hash]} ]]; then
             file_hash_map[$hash]+=$'\n'"$file"
         else
---
+++ remove_duplicates.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+source functions.sh
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 /path/to/checksums.md5"
+    exit 1
+fi
+
+checksums_file="$1"
+
+if [ ! -f "$checksums_file" ]; then
+  echo "$checksums_file not found."
+  exit 1
+fi
+
+directory_path=$(dirname "$checksums_file")
+generate_hash_map "$checksums_file"
+
+for hash in "${!file_hash_map[@]}"; do
+  files=()
+  while IFS= read -r file; do
+    files+=("$file")
+  done <<< "${file_hash_map[$hash]}"
+
+  if [ "${#files[@]}" -gt 1 ]; then
+    shortest_file=$(printf "%s\n" "${files[@]}" | awk '{print length, $0}' | sort -n | cut -d' ' -f2- | head -n 1)
+    for file in "${files[@]}"; do
+      if [ "$file" != "$shortest_file" ]; then
+        full_path_file="${directory_path}/${file}"
+        echo "Removing: $full_path_file"
+        rm "$full_path_file"
+      fi
+    done
+  fi
+done
