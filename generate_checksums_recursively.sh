fb4006fedd29c8e78a0284c292766aa28bebcac2
Pavel Gev <pavelgeveiler@gmail.com>
2024-03-13 12:46:13 +0100

    Erase reference to the deleted file in checksums.md5

--- generate_checksums_recursively.sh
+++ generate_checksums_recursively.sh
@@ -9,7 +9,7 @@ fi
 
 find "$root_dir" -type d -print0 | while IFS= read -r -d $'\0' dir; do
   cd "$dir"
-  checksums=$(md5sum * 2> /dev/null)
+  checksums=$(md5sum * 2> /dev/null | grep -v "checksums.md5")
 
   if [ -n "$checksums" ]; then
     echo "$checksums" > checksums.md5
