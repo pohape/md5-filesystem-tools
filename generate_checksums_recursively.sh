7d4a0b46d9c3e7f3e3213e55e7dbbf8e3ee7874f
Pavel Gev <pavelgeveiler@gmail.com>
2024-03-13 17:22:13 +0100

    Update generate_checksums_recursively.sh

--- generate_checksums_recursively.sh
+++ generate_checksums_recursively.sh
@@ -7,12 +7,25 @@ if [ -z "$root_dir" ]; then
   exit 1
 fi
 
-find "$root_dir" -type d -print0 | while IFS= read -r -d $'\0' dir; do
+directory_list=$(find "$root_dir" -type d -print)
+directory_count=$(echo "$directory_list" | grep -c '^')
+
+echo -e "Directories found:\n$directory_count"
+current_directory_number=0
+
+while IFS= read -r dir; do
+  ((current_directory_number++))
   cd "$dir"
-  checksums=$(md5sum * 2> /dev/null | grep -v "checksums.md5")
+  echo
+  echo "Processing directory $current_directory_number/$directory_count: $dir"
+  echo "The directory has this amount of files:"
+  ls -p | grep -v / | grep -v checksums.md5 | wc -l
+  
+  checksums=$(md5sum ./* 2> /dev/null | grep -v "checksums.md5")
 
   if [ -n "$checksums" ]; then
     echo "$checksums" > checksums.md5
+    echo "Checksums saved: "
+    cat checksums.md5 | wc -l
   fi
-  cd - > /dev/null
-done
+done <<< "$directory_list"
