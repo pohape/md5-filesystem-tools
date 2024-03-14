b9d477fc915d5552df70abba8ebd8be76dee0728
Pavel Gev <pavelgeveiler@gmail.com>
2024-03-14 16:08:03 +0100

    Update find_duplicates_recursively.sh

--- find_duplicates_recursively.sh
+++ find_duplicates_recursively.sh
@@ -16,9 +16,28 @@ fi
 declare -A file_hash_map
 
 mapfile -t checksum_files < <(find "$directory_path" -name 'checksums.md5')
+
 for checksums_file in "${checksum_files[@]}"; do
   echo "Processing $checksums_file"
-  generate_hash_map "$checksums_file"
+  generate_hash_string "$checksums_file"
+  
+  while IFS= read -r item1; do
+      shortest_path=""
+      shortest_len=-1
+      hash=""
+      
+      while IFS= read -r item2; do
+          if [[ "$item2" != /* ]]; then
+            hash="$item2"
+          else
+            if [[ -n ${file_hash_map[$hash]} ]]; then
+              file_hash_map[$hash]+=$'\n'"$item2"
+            else
+              file_hash_map[$hash]=$item2
+            fi
+          fi
+      done < <(echo "$item1" | awk -v RS='!!__DELIMITER1__!!' '{print $0}')
+  done < <(echo "$hash_string" | awk -v RS='!!__DELIMITER2__!!' '{print $0}')
 done
 
 for hash in "${!file_hash_map[@]}"; do
