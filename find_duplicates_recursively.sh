b9d3d84b7e44c48daf1bc7140190ebe4da6bb259
Pavel Gev <pavelgeveiler@gmail.com>
2024-03-14 16:47:51 +0100

    find_duplicates_recursively.sh

--- find_duplicates_recursively.sh
+++ find_duplicates_recursively.sh
@@ -6,46 +6,12 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-directory_path="$1"
+find_duplicates_recursively $1
 
-if [ ! -d "$directory_path" ]; then
-  echo "$directory_path is not a directory."
-  exit 1
-fi
+echo
 
-declare -A file_hash_map
-
-mapfile -t checksum_files < <(find "$directory_path" -name 'checksums.md5')
-
-for checksums_file in "${checksum_files[@]}"; do
-  echo "Processing $checksums_file"
-  generate_hash_string "$checksums_file"
-  
-  while IFS= read -r item1; do
-      shortest_path=""
-      shortest_len=-1
-      hash=""
-      
-      while IFS= read -r item2; do
-          if [[ "$item2" != /* ]]; then
-            hash="$item2"
-          else
-            if [[ -n ${file_hash_map[$hash]} ]]; then
-              file_hash_map[$hash]+=$'\n'"$item2"
-            else
-              file_hash_map[$hash]=$item2
-            fi
-          fi
-      done < <(echo "$item1" | awk -v RS='!!__DELIMITER1__!!' '{print $0}')
-  done < <(echo "$hash_string" | awk -v RS='!!__DELIMITER2__!!' '{print $0}')
-done
-
-for hash in "${!file_hash_map[@]}"; do
-  files="${file_hash_map[$hash]}"
-  if [[ $(grep -c . <<< "$files") -gt 1 ]]; then
-    echo -e "\n$hash:"
-    echo "$files" | while IFS= read -r file; do
-      echo "$file"
-    done
-  fi
-done
+while IFS= read -r item1; do
+    while IFS= read -r item2; do
+        echo $item2
+    done < <(echo "$item1" | awk -v RS='!!__DELIMITER1__!!' '{print $0}')
+done < <(echo "$global_hash_string" | awk -v RS='!!__DELIMITER2__!!' '{print $0}')
--- functions.sh
+++ functions.sh
@@ -13,7 +13,7 @@ generate_hash_string() {
 
         if [[ "$hash" != "$previous_hash" ]]; then
             if [[ -n "$previous_hash" ]]; then
-                hash_string+=$'!!__DELIMITER2__!!'  # Добавляем новую строку перед началом нового хеша
+                hash_string+=$'!!__DELIMITER2__!!'
             fi
             hash_string+="${hash}${delimiter}${full_path_file}"
         else
@@ -55,6 +55,63 @@ process_checksums_file() {
     generate_hash_string "$input_path"
 }
 
+find_duplicates_recursively() {
+    directory_path="$1"
+
+    if [ ! -d "$directory_path" ]; then
+    echo "$directory_path is not a directory."
+    exit 1
+    fi
+
+    delimiter1="!!__DELIMITER1__!!"
+    delimiter2="!!__DELIMITER2__!!"
+
+    declare -A file_hash_map
+    global_hash_string=""
+
+    mapfile -t checksum_files < <(find "$directory_path" -name 'checksums.md5')
+
+    for checksums_file in "${checksum_files[@]}"; do
+    echo "Processing $checksums_file"
+    generate_hash_string "$checksums_file"
+    
+    while IFS= read -r item1; do
+        shortest_path=""
+        shortest_len=-1
+        hash=""
+        
+        while IFS= read -r item2; do
+            if [[ "$item2" != /* ]]; then
+                hash="$item2"
+            else
+                if [[ -n ${file_hash_map[$hash]} ]]; then
+                file_hash_map[$hash]+=$'\n'"$item2"
+                else
+                file_hash_map[$hash]=$item2
+                fi
+            fi
+        done < <(echo "$item1" | awk -v RS=$delimiter1 '{print $0}')
+    done < <(echo "$hash_string" | awk -v RS=$delimiter2 '{print $0}')
+    done
+
+    for hash in "${!file_hash_map[@]}"; do
+    files="${file_hash_map[$hash]}"
+
+    if [[ $(grep -c . <<< "$files") -gt 1 ]]; then
+        if [[ -n $global_hash_string ]]; then
+        global_hash_string+="$delimiter2"
+        fi
+
+        global_hash_string+="$hash"
+        
+        while IFS= read -r file; do
+        global_hash_string+="$delimiter1"
+        global_hash_string+="$file"
+        done < <(echo "$files")
+    fi
+    done
+}
+
 remove_duplicates() {
     local serialized_map=$1
     local -A deserialized_map
@@ -68,13 +125,11 @@ remove_duplicates() {
         IFS=' ' read -r -a files <<< "${deserialized_map[$hash]}"
 
         if [ "${#files[@]}" -gt 1 ]; then
-            # Сортировка файлов по длине имени и выбор самого короткого
             shortest_file=$(printf "%s\n" "${files[@]}" | awk '{print length, $0}' | sort -n | cut -d' ' -f2- | head -n 1)
             echo "The file with the shortest name will be preserved:"
             echo "$shortest_file"
             echo
 
-            # Удаление файлов, которые не являются самым коротким
             for file in "${files[@]}"; do
                 if [ "$file" != "$shortest_file" ]; then
                     echo "Removing: $file"
