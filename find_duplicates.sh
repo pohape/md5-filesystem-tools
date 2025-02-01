b6f0994b041246300a5531592d8ec0743dc22793
Pavel Gev <pavelgeveiler@gmail.com>
2025-02-01 17:20:19 +0100

    Update find_duplicates.sh

--- find_duplicates.sh
+++ find_duplicates.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 source "$(dirname "${BASH_SOURCE[0]}")/functions.sh"
 
-process_checksums_file "$1"
+directory_path="$1"
+echo "Directory Path: $directory_path"
+process_checksums_file "$directory_path"
+echo
+echo
 
 while IFS= read -r item1; do
+    files=()
+
     while IFS= read -r item2; do
-        echo $item2
+        if [[ "$item2" == /* ]]; then
+            files+=("$item2")
+        fi
     done < <(echo "$item1" | awk -v RS='!!__DELIMITER1__!!' '{print $0}')
+
+    if [ "${#files[@]}" -gt 1 ]; then
+        echo "Duplicates:"
+        echo
+
+        for file in "${files[@]}"; do
+            echo "$file"
+        done
+
+        echo "-------------------"
+    fi
+
 done < <(echo "$hash_string" | awk -v RS='!!__DELIMITER2__!!' '{print $0}')
