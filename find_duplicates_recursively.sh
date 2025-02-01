14d4581302e6e80eba4151064429b7834b0ea718
Pavel Gev <pavelgeveiler@gmail.com>
2025-02-01 17:33:38 +0100

    Update find_duplicates_recursively.sh

--- find_duplicates_recursively.sh
+++ find_duplicates_recursively.sh
@@ -6,7 +6,9 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-find_duplicates_recursively $1
+directory_path="$1"
+echo "Directory Path: $directory_path"
+find_duplicates_recursively "$directory_path"
 
 echo
 
