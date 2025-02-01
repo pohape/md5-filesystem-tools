b1b76855c1ef776f24fc3fd28ea675edaaacf73d
Pavel Gev <pavelgeveiler@gmail.com>
2025-02-01 17:35:45 +0100

    Update find_duplicates_recursively.sh

--- find_duplicates_recursively.sh
+++ find_duplicates_recursively.sh
@@ -8,12 +8,38 @@ fi
 
 directory_path="$1"
 echo "Directory Path: $directory_path"
-find_duplicates_recursively "$directory_path"
-
 echo
 
+find_duplicates_recursively "$directory_path"
+echo
+
+duplicates_found=0
+
 while IFS= read -r item1; do
+    files=()
+
     while IFS= read -r item2; do
-        echo $item2
+        if [[ "$item2" == /* ]]; then
+            files+=("$item2")
+        fi
     done < <(echo "$item1" | awk -v RS='!!__DELIMITER1__!!' '{print $0}')
+
+    if [ "${#files[@]}" -gt 1 ]; then
+        ((duplicates_found++))
+
+        echo "🗂️  Duplicate #$duplicates_found:"
+        echo "-----------------------------"
+
+        for file in "${files[@]}"; do
+            echo "📄 $file"
+        done
+
+        echo "-----------------------------"
+        echo
+    fi
+
 done < <(echo "$global_hash_string" | awk -v RS='!!__DELIMITER2__!!' '{print $0}')
+
+if [ "$duplicates_found" -eq 0 ]; then
+    echo "✅ No duplicates found."
+fi
